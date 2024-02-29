36888e88ddf4fe4e155ad9dd0741e26f49e120a3
Egemen Aras <egemenmutluaras@gmail.com>
2024-02-29 04:04:20 -0500

    iterate through whole JSInp files

--- scripts/run_saphsolve/main.py
+++ scripts/run_saphsolve/main.py
@@ -1,26 +1,41 @@
 """"``main.py`` is a Python script that demonstrates how to call the SolveFromInput function from the SolverSaphire.dll"""
 
 from ctypes import *
+import os
 
 try:
     # Load the DLL
-    mydll = cdll.LoadLibrary("C:\\Users\\egeme\\Saphire 8\\tools\\SolverSaphire.dll")
+    mydll = cdll.LoadLibrary(r"C:\Users\egeme\Saphire 8\tools\SolverSaphire.dll")
 except OSError as e:
     print("Error:", e)
     exit(1)  # Exit the script if DLL loading fails
 
-try:
-    # Call the SolveFromInput function from the DLL
-    JSInp_file_path = r"C:\Users\egeme\Desktop\repo\Gitlab\Enhancement-of-PRA-Tools\Model-Exchange\model-converter\saphsolve_actual_models\EQK-BIN1_et_Grp-1_24-02-26_15-57-10.JSInp"
-    JSCut_file_path = r"C:\Users\egeme\Desktop\repo\Gitlab\Enhancement-of-PRA-Tools\Model-Exchange\model-converter\saphsolve_actual_models\EQK-BIN1_et_Grp-1_24-02-26_15-57-10.JSCut"
-    mydll.SolveFromInput(JSInp_file_path, JSCut_file_path)
-    print("Done")  # Print a message indicating successful completion
-except OSError as e:
-    print("Error:", e)
-    exit(2)  # Exit the script if SolveFromInput fails
 
-# from ctypes import*
-# # give location of dll
-# mydll = cdll.LoadLibrary("C:\\Users\\egeme\\Saphire 8\\tools\\SolverSaphire.dll")
-# mydll.SolveFromInput("C:\\Users\\egeme\\Downloads\\LLOCA.JSInp", "C:\\Users\\egeme\\Downloads\\LLOCA.JSCut")
-# print("Done2")
+def solve_json_file(input_file_path, output_file_path):
+    try:
+        # Call the SolveFromInput function from the DLL
+        mydll.SolveFromInput(input_file_path, output_file_path)
+        print(f"Solved {input_file_path}")  # Print a message indicating successful completion
+    except OSError as e:
+        print("Error:", e)
+        exit(2)  # Exit the script if SolveFromInput fails
+
+
+def main():
+    input_directory = r"C:\Users\egeme\Desktop\repo\Gitlab\Enhancement-of-PRA-Tools\Model-Exchange\model-converter\dumped_saphsolve_actual_models"
+    output_directory = r"C:\Users\egeme\Desktop\repo\Gitlab\Enhancement-of-PRA-Tools\Model-Exchange\model-converter\output_dumped_saphsolve_actual_models"
+
+    # List all JSON files in the input directory
+    json_files = [file for file in os.listdir(input_directory) if file.endswith('.JSInp')]
+
+    # Solve each JSON file
+    for file_name in json_files:
+        input_file_path = os.path.join(input_directory, file_name)
+        output_file_path = os.path.join(output_directory, f'dumped_{file_name}')
+
+        # Solve the JSON file
+        solve_json_file(input_file_path, output_file_path)
+
+
+if __name__ == "__main__":
+    main()
