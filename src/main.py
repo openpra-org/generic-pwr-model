1b921c02918f8f5f0f15e17a8948f29d31c0818a
Egemen Aras <egemenmutluaras@gmail.com>
2024-02-29 04:59:17 -0500

    parsing whole JSInp files after validating

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import os
 from JSInp_parser import JSONParser
 from JSInp_dumper import JSONDumper
+import json
 
 def main():
     input_directory = '/Users/afshar-flow/Repo/Gitlab/Enhancement-of-PRA-Tools/Model-Exchange/model-converter/saphsolve_actual_models/'
@@ -14,13 +15,21 @@ def main():
         input_file_path = os.path.join(input_directory, file_name)
         output_file_path = os.path.join(output_directory, f'dumped_{file_name}')
 
-        # Parse the JSON file
-        json_parser = JSONParser(input_file_path)
-        parsed_saphsolve_input_object = json_parser.parse_to_object()
+        print(f"Processing file: {input_file_path}")
 
-        # Dump the parsed data back into another JSON file
-        json_dumper = JSONDumper(parsed_saphsolve_input_object)
-        json_dumper.dump_to_json(output_file_path)
+        # Validate the JSON file
+        try:
+            with open(input_file_path, 'r') as file:
+                json.load(file)
+            # Parse the JSON file if it's valid
+            json_parser = JSONParser(input_file_path)
+            parsed_saphsolve_input_object = json_parser.parse_to_object()
+            # Dump the parsed data back into another JSON file
+            json_dumper = JSONDumper(parsed_saphsolve_input_object)
+            json_dumper.dump_to_json(output_file_path)
+            print(f"File {file_name} parsed and dumped successfully.")
+        except json.JSONDecodeError as e:
+            print(f"Error in file {file_name}: {e}. Skipping parsing and dumping.")
 
 if __name__ == "__main__":
     main()
