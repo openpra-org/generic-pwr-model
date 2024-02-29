61cfa69258d0e2a52340aeb7774396b029f8bfd5
Egemen Aras <egemenmutluaras@gmail.com>
2024-02-29 04:04:48 -0500

    parse via iterate through whole JSInp files

--- src/main.py
+++ src/main.py
@@ -1,20 +1,26 @@
+import os
 from JSInp_parser import JSONParser
 from JSInp_dumper import JSONDumper
 
 def main():
-    JSInp_file_path = '/Users/afshar-flow/Repo/Gitlab/Enhancement-of-PRA-Tools/Model-Exchange/model-converter/saphsolve_actual_models/EQK-BIN1_et_Grp-1_24-02-26_15-57-10.JSInp'
-    dumped_JSInp_file_path = '/Users/afshar-flow/Repo/Gitlab/Enhancement-of-PRA-Tools/Model-Exchange/model-converter/dumped_saphsolve_actual_models/dumped_EQK-BIN1_et_Grp-1_24-02-26_15-57-10.JSInp'
+    input_directory = '/Users/afshar-flow/Repo/Gitlab/Enhancement-of-PRA-Tools/Model-Exchange/model-converter/saphsolve_actual_models/'
+    output_directory = '/Users/afshar-flow/Repo/Gitlab/Enhancement-of-PRA-Tools/Model-Exchange/model-converter/dumped_saphsolve_actual_models/'
 
-    # Parse the JSON file
-    json_parser = JSONParser(JSInp_file_path)
-    # parsed_saphsolve_input = json_parser.parse_from_json()
+    # List all JSON files in the input directory
+    json_files = [file for file in os.listdir(input_directory) if file.endswith('.JSInp')]
 
-    # Convert parsed data to Python objects
-    parsed_saphsolve_input_object = json_parser.parse_to_object()
+    # Parse each JSON file and dump it
+    for file_name in json_files:
+        input_file_path = os.path.join(input_directory, file_name)
+        output_file_path = os.path.join(output_directory, f'dumped_{file_name}')
 
-    # Dump the parsed data back into another JSON file
-    json_dumper = JSONDumper(parsed_saphsolve_input_object)
-    json_dumper.dump_to_json(dumped_JSInp_file_path)
+        # Parse the JSON file
+        json_parser = JSONParser(input_file_path)
+        parsed_saphsolve_input_object = json_parser.parse_to_object()
+
+        # Dump the parsed data back into another JSON file
+        json_dumper = JSONDumper(parsed_saphsolve_input_object)
+        json_dumper.dump_to_json(output_file_path)
 
 if __name__ == "__main__":
     main()
