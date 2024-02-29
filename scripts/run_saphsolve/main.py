d5b3084829c046d56fe950f464f61c99f39346f3
egemen-windows <egemenmutluaras@gmail.com>
2024-02-28 21:51:36 -0500

    change exit code to "2"

--- scripts/run_saphsolve/main.py
+++ scripts/run_saphsolve/main.py
@@ -11,13 +11,13 @@ except OSError as e:
 
 try:
     # Call the SolveFromInput function from the DLL
-    JSInp_file_path = r"C:\Users\egeme\Desktop\repo\Gitlab\Enhancement-of-PRA-Tools\Model-Exchange\model-converter\saphsolve_actual_models\EQK-BIN1_et_Grp-1_24-02-26_15-57-10.JSInp"
+    JSInp_file_path = r"C:\Users\egeme\Desktop\repo\Gitlab\Enhancement-of-PRA-Tools\Model-Exchange\model-converter\dumped_saphsolve_actual_models\dumped_EQK-BIN1_et_Grp-1_24-02-26_15-57-10.JSInp"
     JSCut_file_path = r"C:\Users\egeme\Desktop\repo\Gitlab\Enhancement-of-PRA-Tools\Model-Exchange\model-converter\dumped_saphsolve_actual_models\dumped_EQK-BIN1_et_Grp-1_24-02-26_15-57-10.JSCut"
     mydll.SolveFromInput(JSInp_file_path, JSCut_file_path)
     print("Done")  # Print a message indicating successful completion
 except OSError as e:
     print("Error:", e)
-    exit(1)  # Exit the script if SolveFromInput fails
+    exit(2)  # Exit the script if SolveFromInput fails
 
 # from ctypes import*
 # # give location of dll
