acb473965569eb1903bc29008147052861ffe721
egemen-windows <egemenmutluaras@gmail.com>
2024-02-29 09:22:24 -0500

    added output folder

--- scripts/run_saphsolve/main.py
+++ scripts/run_saphsolve/main.py
@@ -9,10 +9,11 @@ except OSError as e:
     exit(1)  # Exit the script if DLL loading fails
 
 
-def solve_json_file(input_file_path):
+def solve_json_file(input_file_path, output_directory):
     try:
         # Generate the output file path with the .JSCut extension
-        output_file_path = os.path.splitext(input_file_path)[0] + ".JSCut"
+        output_file_name = os.path.splitext(os.path.basename(input_file_path))[0] + ".JSCut"
+        output_file_path = os.path.join(output_directory, output_file_name)
 
         # Call the SolveFromInput function from the DLL
         mydll.SolveFromInput(input_file_path, output_file_path)
@@ -23,16 +24,21 @@ def solve_json_file(input_file_path):
 
 def main():
     input_directory = r"C:\Users\egeme\Desktop\repo\Gitlab\Enhancement-of-PRA-Tools\Model-Exchange\model-converter\dumped_saphsolve_actual_models"
+    output_directory = r"C:\Users\egeme\Desktop\repo\Gitlab\Enhancement-of-PRA-Tools\Model-Exchange\model-converter\output_dumped_saphsolve_actual_models"
 
     # List all JSON files in the input directory
     json_files = [file for file in os.listdir(input_directory) if file.endswith('.JSInp')]
+
+    # Ensure the output directory exists; if not, create it
+    if not os.path.exists(output_directory):
+        os.makedirs(output_directory)
 
     # Solve each JSON file
     for file_name in json_files:
         input_file_path = os.path.join(input_directory, file_name)
 
         # Solve the JSON file
-        solve_json_file(input_file_path)
+        solve_json_file(input_file_path, output_directory)
 
 
 if __name__ == "__main__":
